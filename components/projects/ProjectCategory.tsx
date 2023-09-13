f17572fb70b9b856324d987868a90d59ac35b503
Lougie <lougie.quisel@gmail.com>
2023-09-13 21:43:51 +0800

    Categorize projects

---
+++ components/projects/ProjectCategory.tsx
@@ -0,0 +1,26 @@
+import { ProjectType } from "@/types";
+import Heading from "../common/Heading";
+import Project from "./Project";
+
+type ProjectCategoryType = {
+  name: string;
+  projects: ProjectType[];
+};
+
+export default function ProjectCategory({
+  name,
+  projects,
+}: ProjectCategoryType) {
+  return (
+    <div className="mb-16 w-full sm:mb-20 md:mb-24">
+      <h2 className="mb-5 font-anek-bangla text-3xl font-semibold text-raisin-black sm:text-4xl ">
+        {name}
+      </h2>
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-14">
+        {projects.map((project: ProjectType) => {
+          return <Project project={project} key={project.name} />;
+        })}
+      </div>
+    </div>
+  );
+}
