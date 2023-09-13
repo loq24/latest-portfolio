f78373d5a1e3c56a01cd60e950fb7acb65c0940e
Lougie <lougie.quisel@gmail.com>
2023-09-13 22:42:44 +0800

    Fix project page not scrolling to the top on page load

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,21 +1,24 @@
+import ScrollToTop from "@/components/ScrollToTop";
 import Heading from "@/components/common/Heading";
 import Text from "@/components/common/Text";
 import Projects from "@/components/projects/Projects";
-import React from "react";
 
 export default function page() {
   return (
-    <div className="h-full w-full bg-slate-100 pb-28 pt-44">
-      <div className="centered-block flex flex-col">
-        <Heading as="h1" className="mb-2">
-          All Projects
-        </Heading>
-        <Text className="text-neutral-500">
-          These are the complete list of my projects that I worked on over the
-          years.
-        </Text>
-        <Projects />
+    <>
+      <ScrollToTop />
+      <div className="h-full w-full bg-slate-100 pb-28 pt-44">
+        <div className="centered-block flex flex-col">
+          <Heading as="h1" className="mb-2">
+            All Projects
+          </Heading>
+          <Text className="text-neutral-500">
+            These are the complete list of my projects that I worked on over the
+            years.
+          </Text>
+          <Projects />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
