28ff01ac1d0ef9bfa6950e3ddc6c518289c8d416
Lougie <lougie.quisel@gmail.com>
2023-09-14 12:57:34 +0800

    Hide picture button on scroll

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -6,7 +6,10 @@ export default function Nav() {
   return (
     <div className="fixed left-0 right-0 z-50 py-8 drop-shadow-sm sm:top-8">
       <div className="centered-block relative flex flex-wrap items-center justify-between">
-        <Link className="shrink-0" href="/">
+        <Link
+          className="shrink-0 duration-300 group-[.scrolling]:opacity-0"
+          href="/"
+        >
           <div className="h-[50px] w-[50px] rounded-full bg-[url('/images/lougiequisel.png')] bg-cover ring-4 ring-slate-200"></div>
         </Link>
         <ul className="relative z-20 hidden flex-wrap justify-between py-3 font-inter tracking-wide text-raisin-black duration-300 group-[.scrolling]:opacity-0 sm:flex">
