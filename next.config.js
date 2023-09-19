70132bd3d3d334adcf0aac76a081af52e01a34a1
Lougie <lougie.quisel@gmail.com>
2023-09-19 12:52:12 +0800

    Redirect previous project link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/recent-projects",
+        destination: "/projects",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
