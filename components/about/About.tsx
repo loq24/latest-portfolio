edab57c7b72c3dfb0fd32d1c807d68bf2ec452d5
Lougie <lougie.quisel@gmail.com>
2024-07-19 19:14:19 +0800

    Update about text

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -13,10 +13,10 @@ export default function About() {
       <div className="mb-9 w-full lg:mb-0 lg:w-1/2">
         <Heading>About Me</Heading>
         <Text className="mb-4">
-          I'm passionate about building stunning and user-friendly websites and
-          web applications with a journey spanning more than 12 years in the
-          ever-evolving digital landscape. My stack revolves around the ReactJS
-          ecosystem.
+          I&apos;m a passionate frontend web developer with over 12 years of
+          experience navigating the ever-evolving digital landscape. My
+          expertise lies in the ReactJS ecosystem, where I excel at building
+          dynamic and responsive user interfaces.
         </Text>
         <Text>
           Over the years, I&apos;ve had the privilege of building websites for
