2068fa81102e065f639ce6fda918475a2c9e930c
Lougie Quisel <quisel.lougie24@gmail.com>
2025-05-17 16:18:20 +0800

    feat: adjust margin-bottom

--- components/featured-projects/FeaturedProjects.tsx
+++ components/featured-projects/FeaturedProjects.tsx
@@ -8,7 +8,9 @@ export default function Projects() {
   return (
     <div className="w-full bg-slate-100 py-10 md:py-20" id="projects">
       <div className="centered-block mb-14 flex flex-col justify-center sm:mb-24 md:mb-32">
-        <Heading className="mb-4 text-center">Featured Projects</Heading>
+        <Heading className="mb-4 text-center lg:mb-4">
+          Featured Projects
+        </Heading>
         <Text className="text-center text-sm text-neutral-500">
           Here are my recent featured projects that I&apos;m very proud of.
         </Text>
